04eed35b8a646ecb65541b545f0b432921c40e21
Moaz Mohamed Ali Hamed Qassem <1233255@dtu.edu.eg>
2026-05-09 00:50:11 +0300

    Add files via upload

---
+++ api/salary/get_salary.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+header('Content-Type: application/json; charset=utf-8');
+require_once __DIR__ . '/../../api/Middleware.php';
+authMiddleware();
+$file=__DIR__ . '/../../api/salary/salary.json';
+
+if(!file_exists($file)){
+     echo json_encode([
+        "status" => "error",
+        "message" => "Salary file not found"
+    ]);
+    exit;
+}
+
+$loaddata = file_get_contents($file);
+$all = json_decode($loaddata, true);
+
+if(!$all){
+    $all = [];
+}
+
+$user_id = $_SESSION["email"];
+
+if(!$user_id){
+    echo json_encode([
+        "status" => "error",
+        "message" => "User not authenticated",
+        "salary" => []
+    ]);
+    exit;
+}
+
+foreach($all as $item){
+    if($item["email"] == $user_id){
+        $usersalary = $item["salary"];
+        break;
+    }
+}
+
+echo json_encode([
+    "status" => "success",
+    "salary" => $usersalary,
+]);
+
+?>
